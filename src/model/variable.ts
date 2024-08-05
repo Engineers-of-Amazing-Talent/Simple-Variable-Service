8bb79417d6d0cc954d70d176f4be6f8ebc114a13
JacobKnaack <jacobknck@gmail.com>
2024-08-04 17:32:27 -0700

    adds tests for variable updates and type validation

--- src/model/variable.ts
+++ src/model/variable.ts
@@ -2,6 +2,7 @@ import { DataTypes, Model, Optional } from 'sequelize';
 import { Schema } from './Repository';
 
 export interface VariableAttributes {
+  id?: string,
   type: string;
   key: string;
   value?: string;
@@ -15,6 +16,24 @@ export interface VariableInstance extends Model<VariableAttributes, VariableCrea
 }
 
 const allowedTypes = ['STRING', 'BOOLEAN', 'INTEGER', 'FLOAT', 'LIST'];
+
+function validateFloatValue(value: string) {
+  if (!/^\d+\.\d+$/.test(value)) {
+    throw new Error('Variable of type FLOAT must contain a value in decimal point format.');
+  }
+}
+
+function validateIntegerValue(value: string) {
+  if (!/^\d+$/.test(value)) {
+    throw new Error('Variable of type INTEGER must contain an integer value.');
+  }
+}
+
+function validateBooleanValue(value:string) {
+  if (!/^(true|false)$/i.test(value)) {
+    throw new Error('Value must be either "True" or "False" (case insensitive).');
+  }
+}
 
 export const variableSchema: Schema = {
   name: 'Variable',
@@ -43,9 +62,24 @@ export const variableSchema: Schema = {
       type: DataTypes.TEXT('long'),
       allowNull: true,
       validate: {
-        isNullable(value: unknown) {
+        isNullable(value: string) {
           if (this.type !== 'LIST' && !value) {
             throw new Error('Value can only be null if Type is set to List');
+          }
+        },
+        validateFloat (value: string) {
+          if (this.type === 'FLOAT') {
+            validateFloatValue(value);
+          }
+        },
+        validateInteger(value: string) {
+          if (this.type === 'INTEGER') {
+            validateIntegerValue(value);
+          }
+        },
+        validateBoolean(value: string) {
+          if (this.type === 'BOOLEAN') {
+            validateBooleanValue(value);
           }
         }
       }
--- src/test/model.test.ts
+++ src/test/model.test.ts
@@ -89,7 +89,6 @@ describe('Variable Repository', () => {
   test('Should be able to query a List Variable and return all variables associated with the list', async () => {
       const Variable = repository.getModel<VariableInstance>('Variable');
       const ListItem = repository.getModel<ListItemInstance>('ListItem');
-      // Variable.belongsToMany(Variable, { through: ListItem, as: 'ListVariable', foreignKey: 'listId', otherKey: 'resourceId' });
 
       const itemOne = await Variable.create({key: 'one', value: 'TEST_VALUE_ONE', type: 'STRING'});
       const itemTwo = await Variable.create({key: 'two', value: 'TEST_VALUE_TWO', type: 'STRING'});
@@ -107,5 +106,129 @@ describe('Variable Repository', () => {
       expect(listRecord?.ListVariable[1].key).toEqual('two');
       expect(listRecord?.ListVariable[0].value).toEqual('TEST_VALUE_ONE');
       expect(listRecord?.ListVariable[1].value).toEqual('TEST_VALUE_TWO');
+  });
+
+  test('Should be able to update individual variable values and query updated value in a list', async () => {
+    const Variable = repository.getModel<VariableInstance>('Variable');
+    const ListItem = repository.getModel<ListItemInstance>('ListItem');
+
+    const itemOne = await Variable.create({ key: 'first', value: 'TEST_VALUE_ONE', type: 'STRING'});
+    const itemTwo = await Variable.create({ key: 'parent', type: 'LIST' });
+    await ListItem.create({ listId: itemTwo.id, resourceId: itemOne.id });
+
+    let listRecord = await Variable.findByPk(itemTwo.id, {
+      include: [
+        { model: Variable, as: 'ListVariable' }
+      ]
+    });
+    expect(listRecord?.ListVariable[0].value).toEqual('TEST_VALUE_ONE');
+
+    await Variable.update({
+      value: 'TEST_VALUE_UPDATED'
+    }, { where: { id: itemOne.id } });
+
+    listRecord = await Variable.findByPk(itemTwo.id, {
+      include: [
+        { model: Variable, as: 'ListVariable' }
+      ]
+    });
+    expect(listRecord?.ListVariable[0].value).toEqual('TEST_VALUE_UPDATED');
+  });
+
+  // validation
+  test('Throws error for Variables of type FLOAT not using decimal numbers', async () => {
+    const Variable = repository.getModel<VariableInstance>('Variable');
+
+    expect(
+      Variable.create({
+        key: 'float',
+        value: 'not a float',
+        type: 'FLOAT'
+      })
+    ).rejects.toThrow();
+    
+  });
+
+  // validation
+  test('Throws error for Variables of type FLOAT when containing non-numeric values', async () => {
+    const Variable = repository.getModel<VariableInstance>('Variable');
+
+    expect(
+      Variable.create({
+        key: 'float',
+        value: 'not a float 5.4',
+        type: 'FLOAT'
+      })
+    ).rejects.toThrow();
+
+  });
+  test('Variables of type FLOAT are correctly created when formatted properly', async () => {
+    const Variable = repository.getModel<VariableInstance>('Variable');
+
+    let float = await Variable.create({
+      key: 'float',
+      value: '1.23',
+      type: 'FLOAT'
+    });
+  
+    expect(float.value).toEqual('1.23');
+  });
+  test('Throws error when Variables of type INTEGER contain non integer values.', async () => {
+    const Variable = repository.getModel<VariableInstance>('Variable');
+
+    expect(
+      Variable.create({
+        key: 'integer',
+        value: '1.23',
+        type: 'INTEGER'
+      })
+    ).rejects.toThrow();
+
+  });
+  test('Throws error when Variables of type INTEGER contains any text characters.', async () => {
+    const Variable = repository.getModel<VariableInstance>('Variable');
+
+    expect(
+      Variable.create({
+        key: 'integer',
+        value: 'hello 1',
+        type: 'INTEGER'
+      })
+    ).rejects.toThrow();
+
+  });
+  test('Variables of type INTEGER are properly created when valid integer values are present.', async () => {
+    const Variable = repository.getModel<VariableInstance>('Variable');
+
+    let integer = await Variable.create({
+      key: 'integer',
+      value: '543',
+      type: 'INTEGER'
+    });
+   
+    expect(integer.value).toEqual('543');
+  });
+  test('Should throw an error when Variables of type BOOLEAN are not boolean values', async () => {
+    const Variable = repository.getModel<VariableInstance>('Variable');
+
+    expect(
+      Variable.create({
+        key: 'bool',
+        value: 'not true',
+        type: 'BOOLEAN'
+      })
+    ).rejects.toThrow();
+    
+  });
+  test('Variables of type BOOLEAN are properly created when valid boolean values are present.', async () => {
+    const Variable = repository.getModel<VariableInstance>('Variable');
+
+    let integer = await Variable.create({
+      key: 'bool',
+      value: 'False',
+      type: 'BOOLEAN'
+    });
+
+    expect(integer.value).toEqual('False');
   });
 });
